15c93e394b30947c7191ce2128d49888bb42e551
FrancoLomuscioB <f.lomuscioboccardo@uandresbello.edu>
2022-09-08 15:31:25 -0300

    video player y rework QTE

--- Assets/1. Script/Random_Key.cs
+++ Assets/1. Script/Random_Key.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class Random_Key : MonoBehaviour
 {
@@ -9,6 +10,26 @@ public class Random_Key : MonoBehaviour
     private int[] keyList = new int [4];
     public Sprite[] spriteList;
     public GameObject panel;
+    public int correctKey;
+    public int waitingForKey;
+    public int CountingDown;
+    public GameObject videoPlayer;
+    public VideoClip[] videoList;
+    private int videoIndex;
+    public double time;
+    public double currentTime;
+    public bool videoPlaying;
+
+    private void Awake()
+    {
+        videoIndex = 0;
+        waitingForKey = 0;
+        CountingDown = 0;
+        randomValue = 5;
+        videoPlayer.GetComponent<VideoPlayer>().clip = videoList[videoIndex];
+        time = videoPlayer.GetComponent<VideoPlayer>().clip.length;
+        videoPlaying = true;
+    }
     void Start()
     {
         keyList[0] = (int)KeyCode.LeftArrow;
@@ -19,16 +40,98 @@ public class Random_Key : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (videoPlaying)
         {
-            RandomKeyGenerator();
+            currentTime = videoPlayer.GetComponent<VideoPlayer>().time;
+            if(currentTime >= time)
+            {
+                waitingForKey = 1;
+                videoPlaying = false;
+            }
+        }
+        else
+        {
+            if (waitingForKey == 1)
+            {
+                randomValue = Random.Range(0, 4);
+                QuickTimeEventCase(randomValue);
+                CountingDown = 1;
+                waitingForKey = 2;
+            }
+            if (randomValue == 0)
+            {
+                if (Input.anyKeyDown)
+                {
+                    if (Input.GetKeyDown("left"))
+                    {
+                        correctKey = 1;
+                    }
+                    else
+                    {
+                        correctKey = 2;
+                    }
+                    StartCoroutine(KeyPressed());
+                }
+            }
+            if (randomValue == 1)
+            {
+                if (Input.anyKeyDown)
+                {
+                    if (Input.GetKeyDown("right"))
+                    {
+                        correctKey = 1;
+                    }
+                    else
+                    {
+                        correctKey = 2;
+                    }
+                    StartCoroutine(KeyPressed());
+                }
+            }
+            if (randomValue == 2)
+            {
+                if (Input.anyKeyDown)
+                {
+                    if (Input.GetKeyDown("up"))
+                    {
+                        correctKey = 1;
+                    }
+                    else
+                    {
+                        correctKey = 2;
+                    }
+                    StartCoroutine(KeyPressed());
+                }
+            }
+            if (randomValue == 3)
+            {
+                if (Input.anyKeyDown)
+                {
+                    if (Input.GetKeyDown("down"))
+                    {
+                        correctKey = 1;
+                    }
+                    else
+                    {
+                        correctKey = 2;
+                    }
+                    StartCoroutine(KeyPressed());
+                }
+            }
         }
     }
-    void RandomKeyGenerator() 
+
+    IEnumerator KeyPressed()
     {
-        randomValue = Random.Range(0, 4);
-        QuickTimeEventCase(randomValue);
+        randomValue = 4;
+        if (correctKey == 1)
+        {
+            CountingDown = 2;
+            yield return new WaitForSeconds(1.5f);
+            correctKey = 0;
+        }
     }
+
     void QuickTimeEventCase(int e)
     {
         panel.SetActive(true);
