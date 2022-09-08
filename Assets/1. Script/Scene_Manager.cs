50fd3ee6b7a11460bc5c2ad98801acf39f979eef
laakar <laarka1102@gmail.com>
2022-09-07 21:23:24 -0300

    Pantalla game over y font

---
+++ Assets/1. Script/Scene_Manager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Manager : MonoBehaviour
+{
+    public GameObject gameOverPanel;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            gameOverPanel.SetActive(true);
+        }
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene("pruebas", LoadSceneMode.Single);
+    }
+}
